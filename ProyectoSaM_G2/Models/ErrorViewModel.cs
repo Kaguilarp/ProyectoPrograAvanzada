a508837bbb222a9912a1fabfac7e484de95b0150
Kaguilarp <106714015+Kaguilarp@users.noreply.github.com>
2025-07-18 19:11:58 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoSaM_G2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoSaM_G2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
